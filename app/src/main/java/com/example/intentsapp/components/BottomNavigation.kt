4c53cc4ac3f23df5766b552d3da9aa4bf5e6beb5
yan_ortiz <yanortiz@wposs.com>
2024-04-08 15:32:44 -0500

    implementacion de un bottomnavigation y un bottombar

---
+++ app/src/main/java/com/example/intentsapp/components/BottomNavigation.kt
@@ -0,0 +1,48 @@
+package com.example.intentsapp.components
+
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.example.intentsapp.navigation.Routes
+
+@Composable
+fun BottomNav(navHostController: NavHostController, routes: List<Routes>) {
+    BottomAppBar() {
+        NavigationBar(containerColor = Color.White) {
+            val currentRoutes = CurrentRoute(navHostController)
+            routes.forEach { item ->
+                NavigationBarItem(
+                    selected = currentRoutes == item.routes,
+                    onClick = { navHostController.navigate(item.routes) },
+                    icon = {
+                        (Icon(
+                            imageVector = item.icon,
+                            contentDescription = item.title,
+                            tint = Color.Blue
+                        ))
+                    },
+                    label = {
+                        Text(text = item.title)
+                    },
+                    alwaysShowLabel = false
+
+                    )
+
+            }
+        }
+    }
+}
+
+
+@Composable
+fun CurrentRoute(navHostController: NavHostController): String? {
+    val current by navHostController.currentBackStackEntryAsState()
+    return current?.destination?.route
+}
--- app/src/main/java/com/example/intentsapp/views/HomeView.kt
+++ app/src/main/java/com/example/intentsapp/views/HomeView.kt
@@ -1,8 +1,27 @@
 package com.example.intentsapp.views
 
+import android.annotation.SuppressLint
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
+import com.example.intentsapp.components.BottomNav
+import com.example.intentsapp.navigation.NavManager
+import com.example.intentsapp.navigation.Routes
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun HomeView(){
-
+    val navControler = rememberNavController()
+    val navigationRoutes = listOf(
+        Routes.phoneView,
+        Routes.EmailView,
+        Routes.SmsView
+    )
+    Scaffold(
+        bottomBar = {
+           BottomNav(navControler , navigationRoutes )
+        }
+    ) {
+        NavManager(navControler)
+    }
 }
