2f396689f905e248fcd950eea913d023403fe51c
yan_ortiz <yanortiz@wposs.com>
2024-04-08 14:55:14 -0500

    Estructura e implementacion del proyecto

---
+++ app/src/main/java/com/example/intentsapp/views/EmailView.kt
@@ -0,0 +1,9 @@
+package com.example.intentsapp.views
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun EmailView(){
+    Text(text = "EmailView")
+}
---
+++ app/src/main/java/com/example/intentsapp/views/HomeView.kt
@@ -0,0 +1,8 @@
+package com.example.intentsapp.views
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeView(){
+
+}
